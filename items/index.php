ed2785e1e5d722d54a645ef302a3fabb729b132f
kethle <ktleary@gmail.com>
2014-08-20 18:41:57 -0400

    adding flights

--- items/index.php
+++ items/index.php
@@ -1,6 +1,22 @@
 <?php
 
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    //header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');    
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header('Access-Control-Allow-Headers: Content-Type'); 
+    header('Access-Control-Allow-Headers: X-f35e678261fde10f4ce97d1a91881c9f');
+} 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, OPTIONS");         
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+} 
 
 // database connect -- need to substitute with environment variables
 function connectDB(){
@@ -263,8 +279,7 @@ $dbpwd = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
     $quantity=$put_vars["quantity"];
     $price=$put_vars["price"];
 
-    //echo ($item_id . $item_name . $description . $quantity . $price);
-
+   
     $last_id=updateItem($item_id,  $item_name,  $description);
     
     updateItemProperties($item_id,  $description,  $quantity,  $price);
