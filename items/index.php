df2d1921c37ba25186316222d1d7a59b7ea5ad29
Kevin Leary <ktleary@gmail.com>
2014-09-05 22:13:58 -0400

    Update index.php

--- items/index.php
+++ items/index.php
@@ -1,22 +1,6 @@
 <?php
 
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    //header("Access-Control-Allow-Origin: *");
-    header('Access-Control-Allow-Credentials: true');    
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header('Access-Control-Allow-Headers: Content-Type'); 
-    header('Access-Control-Allow-Headers: X-f35e678261fde10f4ce97d1a91881c9f');
-} 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, OPTIONS");         
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-} 
 
 // database connect -- need to substitute with environment variables
 function connectDB(){
